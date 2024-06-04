9858a70259d9a3063839aa33aa08e2d8b39e83fc
legoraft <legoraft@noreply.codeberg.org>
2024-06-04 17:57:06 +0200

    Moved randomization

--- src/randomizer.rs
+++ src/randomizer.rs
@@ -1,22 +1,8 @@
-use std::io;
 use rand::prelude::*;
 
-use crate::parser::Card;
+pub fn generate_indices(length: usize) -> Vec<usize> {
+    let indices: Vec<usize> = (0..length).collect();
+    let indices: Vec<usize> = indices.choose_multiple(&mut thread_rng(), length).cloned().collect();
 
-pub fn recite(cards: &Vec<Card>) {
-    let next_card = thread_rng().gen_range(0..cards.len());
-
-    let term = &cards[next_card].term;
-    let definition = &cards[next_card].definition;
-
-    println!("What does {:?} mean?", term);
-    let mut input = String::new();
-    input.clear();
-    io::stdin().read_line(&mut input).unwrap();
-
-    if &input.to_lowercase().trim() == definition {
-        println!("Well done! {} is indeed {}!", term, definition);
-    } else {
-        println!("Oh well! {} actually means {}", term, definition)
-    }
+    indices
 }
